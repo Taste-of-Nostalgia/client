63c044c8f8fbd07507c04a73974df10e9cad6e4e
ji-mmyliu <jimmyjcl753@gmail.com>
2024-01-28 06:53:24 -0500

    Update recommendations

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -6,9 +6,13 @@ import React from 'react'
 export default function Recommendation() {
   const [recs, setRecs] = React.useState({});
   const [fetched, setFetched] = React.useState(false);
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated, getAccessTokenSilently, loginWithRedirect } = useAuth0();
 
   React.useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+      return;
+    }
     getAccessTokenSilently().then((token) => {
       axios.get('https://api.tasteofnostalgia.tech/recommendations', {
         headers: {
@@ -26,58 +30,57 @@ export default function Recommendation() {
   return (
     <>
       <link rel="stylesheet" href="/output.css" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-red-500 rounded-xl p-8 w-3/4 h-4/5" style={{ height: '95%' }}>
-          <div className="bg-white rounded-xl p-8 w-full h-full flex items-center justify-center">
-            {/* Left Side */}
-            <div className="flex-1 pr-4">
-              <div className="border-r-4 border-gray h-5/6"></div>
-              <h2 className="text-black text-lg font-semibold">Most Similar</h2>
-              <p className="text-sm mb-2">{fetched ? Object.keys(recs)[0] : 'Loading...'}</p>
-              <img
+      <div className="flex items-center justify-center">
+        <div className="bg-white rounded-xl p-8 w-full h-full flex items-center justify-center">
+          {/* Left Side */}
+          <div className="flex-1 pr-4">
+            <div className="border-r-4 border-gray h-5/6"></div>
+            <h2 className="text-black text-lg font-semibold">Most Similar</h2>
+            <p className="text-sm mb-2">{fetched ? Object.keys(recs)[0] : 'Loading...'}</p>
+            {/* <img
                 src={Food}
                 alt="Sample Image"
                 className="object-cover rounded-xl justify-center"
                 style={{ objectFit: 'cover', borderRadius: '0.75rem' }}
-              />
-              <p className="text-xs mt-2">{fetched ? recs[Object.keys(recs)[0]] : 'Loading...'}</p>
-            </div>
+              /> */}
+            <p className="text-xs mt-2">{fetched ? recs[Object.keys(recs)[0]] : 'Loading...'}</p>
+          </div>
 
-            {/* Vertical Bar */}
-            <div className="w-0.5 bg-gray-400 h-full"></div>
+          {/* Vertical Bar */}
+          <div className="w-0.5 bg-gray-400 h-full"></div>
 
-            {/* Middle Side */}
-            <div className="flex-1 pl-4 pr-4">
-              <div className="border-r-4 border-gray h-5/6"></div>
-              <div className="border-l-4 border-gray h-5/6"></div>
-              <h2 className="text-black text-lg font-semibold">Highest Rated</h2>
-              <p className="text-sm mb-2">{fetched ? Object.keys(recs)[1] : 'Loading...'}</p>
-              <img
+          {/* Middle Side */}
+          <div className="flex-1 pl-4 pr-4">
+            <div className="border-r-4 border-gray h-5/6"></div>
+            <div className="border-l-4 border-gray h-5/6"></div>
+            <h2 className="text-black text-lg font-semibold">Highest Rated</h2>
+            <p className="text-sm mb-2">{fetched ? Object.keys(recs)[1] : 'Loading...'}</p>
+            {/* <img
                 src={Food}
                 alt="Sample Image"
                 className="object-cover rounded-xl justify-center"
                 style={{ objectFit: 'cover', borderRadius: '0.75rem' }}
-              />
-              <p className="text-xs mt-2">{fetched ? recs[Object.keys(recs)[1]] : 'Loading...'}</p>
-            </div>
+              /> */}
+            <p className="text-xs mt-2">{fetched ? recs[Object.keys(recs)[1]] : 'Loading...'}</p>
+          </div>
 
-            {/* Vertical Bar */}
-            <div className="w-0.5 bg-gray-400 h-full"></div>
+          {/* Vertical Bar */}
+          <div className="w-0.5 bg-gray-400 h-full"></div>
 
-            {/* Right Side */}
-            <div className="flex-1 pl-4">
-              <div className="border-l-4 border-gray h-5/6"></div>
-              <h2 className="text-black text-lg font-semibold">Most Creative</h2>
-              <p className="text-sm mb-2">{fetched ? Object.keys(recs)[2] : 'Loading...'}</p>
-              <img
+          {/* Right Side */}
+          <div className="flex-1 pl-4">
+            <div className="border-l-4 border-gray h-5/6"></div>
+            {/* <h2 className="text-black text-lg font-semibold">Most Creative</h2> */}
+            {/* <p className="text-sm mb-2">{fetched ? Object.keys(recs)[2] : 'Loading...'}</p> */}
+            {/* <img
                 src={Food}
                 alt="Sample Image"
                 className="object-cover rounded-xl justify-center"
                 style={{ objectFit: 'cover', borderRadius: '0.75rem' }}
-              />
-              <p className="text-xs mt-2">{fetched ? recs[Object.keys(recs)[2]] : 'Loading...'}</p>
-            </div>
-          </div></div></div>
+              /> */}
+            {/* <p className="text-xs mt-2">{fetched ? recs[Object.keys(recs)[2]] : 'Loading...'}</p> */}
+          </div>
+        </div></div>
     </>
 
   );
